c53eadeea753b1b74f85f02dd3269768f1635c2a
lixuejia198 <749134054@qq.com>
2022-04-21 09:40:39 +0800

    调整布局

---
+++ src/components/TopNav.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="login-info">
+    <ul>
+      <template v-if="userInfo.id">
+        <li>
+          <RouterLink to="/my" style="color: #333333">
+            <UserOutlined />{{ userInfo.username }}
+          </RouterLink>
+        </li>
+        <li @click="loginOut">退出登录</li>
+      </template>
+      <template v-else>
+        <li>
+          <RouterLink style="color: #333333" to="/login">请先登录</RouterLink>
+        </li>
+      </template>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import { getUserInfo } from "@/utils/getUserInfo";
+import { useRouter } from "vue-router";
+import { UserOutlined } from "@ant-design/icons-vue";
+
+export default {
+  name: "TopNav",
+  components: { UserOutlined },
+  setup() {
+    const userInfo = ref(getUserInfo());
+    const router = useRouter();
+    // 退出登录
+    const loginOut = () => {
+      window.localStorage.removeItem("community-parking");
+      router.push("/login");
+    };
+
+    return {
+      userInfo,
+      loginOut,
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.login-info {
+  height: 50px;
+  border-bottom: 1px solid #ccc;
+  ul {
+    height: 100%;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding: 0;
+    margin: 0;
+    li {
+      list-style: none;
+      padding: 0 20px 0 6px;
+      border-left: 2px solid #ff7300;
+      font-size: 16px;
+    }
+  }
+}
+</style>
--- src/utils/getUserInfo.js
+++ src/utils/getUserInfo.js
@@ -3,11 +3,10 @@ export function getUserInfo() {
   const token = localStorage.getItem("community-parking");
   // 如果有token 就解构token中的用户信息(用户id和用户名)
   if (token) {
-    const userInfo = JSON.parse(
+    // 返回用户信息(用户id和用户名)
+    return JSON.parse(
       Buffer.from(token.split(".")[1], "base64").toString("utf-8")
     ).data;
-    // 返回用户信息(用户id和用户名)
-    return userInfo;
   }
   return {};
 }
